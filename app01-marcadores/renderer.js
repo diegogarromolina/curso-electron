328764933393de37641460fd0fa12d7b17062826
diego-garro <diego.garromolina@yahoo.com>
2021-03-09 13:32:33 -0600

    Clase 34 terminada

--- app01-marcadores/renderer.js
+++ app01-marcadores/renderer.js
@@ -1,10 +1,80 @@
 class Marcadores {
     constructor() {
-        this.mensajeError = document.getElementsByClassName('mensaje-error')
-        this.marcadorUrl = document.getElementsByClassName('creacion-marcador-url')
-        this.marcadorBoton = document.getElementsByClassName('creacion-marcador-boton')
-        this.formulario = document.getElementsByClassName('creacion-marcador-formulario')
-        this.marcadores = document.getElementsByClassName('marcadores')
-        this.eliminarMarcadores = document.getElementsByClassName('remover-marcadores')
+        this.mensajeError = document.querySelector('.mensaje-error')
+        this.marcadorUrl = document.querySelector('.creacion-marcador-url')
+        this.marcadorBoton = document.querySelector('.creacion-marcador-boton')
+        this.formulario = document.querySelector('.creacion-marcador-formulario')
+        this.marcadores = document.querySelector('.marcadores')
+        this.eliminarMarcadores = document.querySelector('.remover-marcadores')
+
+        this.parser = new DOMParser()
+
+        this.agregarEventListeners()
+    }
+
+    agregarEventListeners() {
+        this.marcadorUrl.addEventListener('keyup', () => {
+            this.marcadorBoton.disabled = !this.marcadorUrl.validity.valid
+        })
+
+        this.formulario.addEventListener('submit', this.crearMarcador.bind(this))
+    }
+
+    crearMarcador(evento) {
+        evento.preventDefault();
+        
+        const url = this.marcadorUrl.value
+
+        fetch(url)
+            .then(respuesta => respuesta.text())
+            .then(this.extraerContenido.bind(this))
+            .then(this.encontrarTituloPagina)
+            .then(titulo => this.almacenarMarcador(url, titulo))
+            .then(this.limpiarFormulario.bind(this))
+            .then(this.visualizarMarcadores.bind(this))
+            .catch(error => this.reportarError(error, url))
+    }
+
+    extraerContenido(contenido) {
+        return this.parser.parseFromString(contenido, 'text/html')
+    }
+
+    encontrarTituloPagina(html) {
+        return html.querySelector('title').innerText
+    }
+
+    almacenarMarcador(url, titulo) {
+        localStorage.setItem(url, JSON.stringify({titulo: titulo, url: url}))
+    }
+
+    limpiarFormulario() {
+        this.marcadorUrl.value = null
+    }
+
+    obtenerMarcadores() {
+        return Object.keys(localStorage).map(k => JSON.parse(localStorage.getItem(k)))
+    }
+
+    generarHtmlMarcador(marcador) {
+        return `<div class="enlace"><h3>${marcador.titulo}</h3>
+        <p><a href="${marcador.url}">${marcador.url}</a></p></div>`
+    }
+
+    visualizarMarcadores() {
+        let marcadores = this.obtenerMarcadores()
+
+        let html =  marcadores.map(value => this.generarHtmlMarcador(value)).join('')
+
+        this.mensajeError.innerHTML = html
+    }
+
+    reportarError(error, url) {
+        this.mensajeError.innerHTML = `Ocurrió un error al intentar acceder a ${url}: ${error}`
+
+        setTimeout(() => {
+            this.mensajeError.innerHTML = null
+        }, 5000)
     }
 }
+
+new Marcadores()
