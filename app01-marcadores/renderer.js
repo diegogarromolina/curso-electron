df637f7efaedf723c87f4b70c8a0f50e09b2e444
diego-garro <diego.garromolina@yahoo.com>
2021-03-09 12:08:46 -0600

    Clase 25 terminada

---
+++ app01-marcadores/renderer.js
@@ -0,0 +1,10 @@
+class Marcadores {
+    constructor() {
+        this.mensajeError = document.getElementsByClassName('mensaje-error')
+        this.marcadorUrl = document.getElementsByClassName('creacion-marcador-url')
+        this.marcadorBoton = document.getElementsByClassName('creacion-marcador-boton')
+        this.formulario = document.getElementsByClassName('creacion-marcador-formulario')
+        this.marcadores = document.getElementsByClassName('marcadores')
+        this.eliminarMarcadores = document.getElementsByClassName('remover-marcadores')
+    }
+}
